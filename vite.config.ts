86866070f86d460dd0a6f721099a098180c80ab0
Joshua Hull <josh.hull.w@gmail.com>
2022-08-28 14:25:52 +1000

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      dts: true,
+      imports: [
+        "vue",
+      ],
+    }),
+    Components({
+      dts: true,
+    }),
+  ],
+});
