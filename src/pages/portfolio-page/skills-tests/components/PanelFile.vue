45bdeedc3cab57bde0ef9a10d26f00913d667edb
Joshua Hull <josh.hull.w@gmail.com>
2023-02-21 22:30:30 +1100

    feat(skills-test): editor content line numbers

--- src/pages/portfolio-page/skills-tests/components/PanelFile.vue
+++ src/pages/portfolio-page/skills-tests/components/PanelFile.vue
@@ -1,11 +1,24 @@
 <template>
 <article class="panel-file" ref="el">
-  <pre class="file-content">{{ panelTestsContent }}</pre>
+  <pre class="file-content">{{ contentWithLineNumbers }}</pre>
 </article>
 </template>
 
 <script setup lang="ts">
 import panelTestsContent from "content:src/pages/portfolio-page/skills-tests/components/PanelTests";
+
+const contentWithLineNumbers = computed(() => {
+  const lines = (<string>panelTestsContent).split("\n");
+
+  let rtn = "";
+
+  for (let i = 0; i < lines.length; i += 1) {
+    const padding = ' '.repeat(`${lines.length}`.length - `${i + 1}`.length);
+    rtn += `${padding}${i + 1} ${lines[i]}\n`;
+  }
+
+  return rtn;
+});
 
 const el = ref<HTMLElement | null>(null);
 
