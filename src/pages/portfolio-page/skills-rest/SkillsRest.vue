48b89c6faae16a48ddb2bdde872655ea82d245fe
Joshua Hull <josh.hull.w@gmail.com>
2023-02-22 21:39:30 +1100

    feat(skills-rest): display restful response messages

--- src/pages/portfolio-page/skills-rest/SkillsRest.vue
+++ src/pages/portfolio-page/skills-rest/SkillsRest.vue
@@ -15,50 +15,121 @@
       @keyup.delete="() => handleDelete()"
     />
   </div>
-  <section class="rest-endpoints" @click="handleBackgroundClick">
-    <button @click="handleCreate">
-      POST
-    </button>
-    <span>/background/blobs</span>
-    <button @click="handleDelete">
-      DELETE
-    </button>
-    <span>/background/blobs/{{ selectedColourText }}</span>
-    <button @click="handleRecolour">
-      POST
-    </button>
-    <span>/background/blobs/{{ selectedColourText }}/re-colour</span>
+  <section class="rest-endpoints">
+    <div class="rest-endpoint">
+      <button
+        class="rest-method"
+        @click="handleCreate"
+      >
+        POST
+      </button>
+      <span
+        class="rest-url"
+      >
+        /background/blobs
+      </span>
+      <span
+        v-if="createMessage"
+        class="rest-message"
+        :style="`color: ${createMessage.colour}`"
+      >
+        {{ createMessage.content }}
+      </span>
+    </div>
+    <div class="rest-endpoint">
+      <button
+        class="rest-method"
+        @click="handleDelete"
+      >
+        DELETE
+      </button>
+      <span
+        class="rest-url"
+      >
+        /background/blobs/{{ selectedColourText }}
+      </span>
+      <span
+        v-if="deleteMessage"
+        class="rest-message"
+        :style="`color: ${deleteMessage.colour}`"
+      >
+        {{ deleteMessage.content }}
+      </span>
+    </div>
+    <div class="rest-endpoint">
+      <button
+        class="rest-method"
+        @click="handleRecolour"
+      >
+        POST
+      </button>
+      <span
+        class="rest-url"
+      >
+        /background/blobs/{{ selectedColourText }}/re-colour
+      </span>
+      <span
+        v-if="recolourMessage"
+        class="rest-message"
+        :style="`color: ${recolourMessage.colour}`"
+      >
+        {{ recolourMessage.content }}
+      </span>
+    </div>
   </section>
 </article>
 </template>
 
 <script setup lang="ts">
+import { useVanishingObject } from "@composables";
+
 type Colour = {
   hex: string;
   left: string;
   top: string;
 };
 
+type Message = {
+  colour: string;
+  content: string;
+};
+
 const selectedColour = ref<string | null>(null);
 const coloursOnTheScreen = ref<Colour[]>([]);
+const [createMessage, pushCreateMessage] = useVanishingObject<Message>(4000);
+const [deleteMessage, pushDeleteMessage] = useVanishingObject<Message>(4000);
+const [recolourMessage, pushRecolourMessage] = useVanishingObject<Message>(4000);
 
 const selectedColourText = computed(() => selectedColour.value ?? ":selected");
+const restEndpointsCursor = computed(() => selectedColour.value ? "pointer" : "default");
 
 function randomColour() {
   return `#${(Math.random() * 0xFFFFFF << 0).toString(16).toUpperCase()}`;
 }
 
 function handleCreate() {
+  const hex = randomColour();
+
   coloursOnTheScreen.value.push({
-    hex: randomColour(),
+    hex,
     left: `${20 + Math.random() * 60}%`,
     top: `${20 + Math.random() * 60}%`,
+  });
+
+  pushCreateMessage({
+    colour: "green",
+    content: `200 OK: created blob with colour "${hex}"`,
   });
 }
 
 function handleDelete() {
-  if (!selectedColour.value)
+  if (!selectedColour.value) {
+    pushDeleteMessage({
+      colour: "red",
+      content: "400 Bad Request: route variable \"selected\" is required",
+    });
     return;
+  }
 
   const idx = coloursOnTheScreen.value.findIndex(c => c.hex === selectedColour.value);
 
@@ -66,12 +137,21 @@ function handleDelete() {
     return;
 
   coloursOnTheScreen.value.splice(idx, 1);
+  pushDeleteMessage({
+    colour: "green",
+    content: `204 No Content: deleted blob with colour "${selectedColour.value}"`,
+  });
   selectedColour.value = null;
 }
 
 function handleRecolour() {
-  if (!selectedColour.value)
+  if (!selectedColour.value) {
+    pushRecolourMessage({
+      colour: "red",
+      content: "400 Bad Request: route variable \"selected\" is required",
+    });
     return;
+  }
 
   const idx = coloursOnTheScreen.value.findIndex(c => c.hex === selectedColour.value);
 
@@ -80,15 +160,15 @@ function handleRecolour() {
 
   const newColour = randomColour();
   coloursOnTheScreen.value[idx].hex = newColour;
+  pushRecolourMessage({
+    colour: "green",
+    content: `200 OK: updated blob colour from "${selectedColour.value}" to "${newColour}"`,
+  });
   selectedColour.value = newColour;
 }
 
 function handleBlobClick(colour: Colour) {
   selectedColour.value = colour.hex;
-}
-
-function handleBackgroundClick() {
-  selectedColour.value = null;
 }
 </script>
 
@@ -119,13 +199,41 @@ function handleBackgroundClick() {
   position: relative;
   z-index: 1;
   display: grid;
-  grid-template-columns: min-content minmax(10px, 22em);
   grid-template-rows: repeat(3, auto);
-  column-gap: 1rem;
-  row-gap: 1rem;
   background-color: black;
-  padding: 1rem;
+  padding: 2rem 2rem 0rem 2rem;
   border-radius: 1rem;
   font-family: monospace;
+}
+
+.rest-endpoint {
+  display: grid;
+  grid-template-columns: minmax(10px, 5rem) minmax(10px, 22em);
+  grid-template-rows: auto 2rem;
+  grid-template-areas:
+    "method     url"
+    "     . message"
+  ;
+  column-gap: 1rem;
+}
+
+.rest-method {
+  grid-area: method;
+}
+
+.rest-url {
+  grid-area: url;
+}
+
+.rest-message {
+  grid-area: message;
+  font-size: 0.5em;
+  color: red;
+}
+</style>
+
+<style scoped>
+.rest-endpoints {
+  cursor: v-bind(restEndpointsCursor);
 }
 </style>
