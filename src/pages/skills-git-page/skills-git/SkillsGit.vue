309d86e634be610f48009e1a51baea7c52610e2c
Joshua Hull <josh.hull.w@gmail.com>
2023-03-13 21:56:28 +1100

    feat(skills-git): self-explain to the user how to use this page

--- src/pages/skills-git-page/skills-git/SkillsGit.vue
+++ src/pages/skills-git-page/skills-git/SkillsGit.vue
@@ -14,6 +14,21 @@ import { Ref } from "vue";
 const fs = new FileSystem();
 const sc = new SourceControl(fs);
 const sourceControl = ref<SourceControl>(sc) as Ref<SourceControl>;
+
+const f1 = fs.addFolder("This is a file explorer.");
+fs.addFolder("Add files and folders");
+fs.addFolder("with the buttons above.");
+fs.addFolder("=================");
+
+f1.addFile("package.json");
+sourceControl.value.stageChange("/This is a file explorer./package.json");
+sourceControl.value.commit("Initial commit");
+
+fs.addFile("Think of unstaged files as");
+fs.addFile("being a work in progress.");
+
+fs.addFile("But staged files are included");
+fs.addFile("in your next commit.");
 </script>
 
 <style>
