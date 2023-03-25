48ff7c4b65c25f1258dcd0201a231d5b0cf29b08
Joshua Hull <josh.hull.w@gmail.com>
2023-03-25 19:51:36 +1100

    feat: use-vanishing-values composable

--- composables/use-vanishing-value/index.ts
+++ composables/use-vanishing-value/index.ts
@@ -1,31 +1,12 @@
-import { computed, ref, Ref } from "vue";
+import { useVanishingValues } from "use-vanishing-values";
+import { computed, Ref } from "vue";
 
 type Push<T> = (value: T) => void;
 type Current<T> = Ref<T | null>;
 
 export function useVanishingValue<T>(vanishAfterMs: number): [Current<T>, Push<T>] {
-  const values = ref<T[]>([]);
+  const [values, push] = useVanishingValues(vanishAfterMs);
   const rtn = computed(() => values.value[0] ?? null);
-
-  function popFirstElementSoon() {
-    if (values.value.length === 0)
-      return;
-
-    setTimeout(() => {
-      values.value = [...values.value.slice(1)];
-      popFirstElementSoon();
-    }, vanishAfterMs);
-  }
-
-  const push = (object: T) => {
-    const identifiedObject = object as T;
-    values.value = [...values.value, (identifiedObject as any)]; // expecting an internal Vue type
-
-    if (values.value.length > 1)
-      return;
-
-    popFirstElementSoon();
-  };
 
   return [rtn as Current<T>, push];
 }
