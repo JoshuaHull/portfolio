6734b0c3f055bdacb730e15573b1ee871ffdc77d
Joshua Hull <josh.hull.w@gmail.com>
2023-03-26 18:44:24 +1100

    chore(skills-messaging): nvm, fold back into one component because vue

--- apps/portfolio/src/pages/skills-messaging-page/components/AnimatedTravellingMessages.vue
+++ apps/portfolio/src/pages/skills-messaging-page/components/AnimatedTravellingMessages.vue
@@ -1,12 +1,12 @@
 <template>
 <TransitionGroup name="travelling-message">
   <template v-for="m of messages" :key="m">
-    <AnimatedTravellingMessage
-      :class="messageClasses"
-      :variant="variant"
-      :animationDuration="animationDuration"
+    <IconContainer
+      :class="`animated-travelling-message ${messageClasses}`"
+      height="1.5rem"
     >
-    </AnimatedTravellingMessage>
+      <EnvelopeSolidIcon />
+    </IconContainer>
   </template>
 </TransitionGroup>
 </template>
@@ -21,15 +21,55 @@ interface AnimatedTravellingMessageProps {
   animationDuration: number | null;
 }
 
-defineProps<AnimatedTravellingMessageProps>();
+const props = defineProps<AnimatedTravellingMessageProps>();
+
+const {
+  animationDuration,
+  messageClasses,
+  messages,
+  variant,
+} = toRefs(props);
+
+const animationDurationInMs = computed(() => `${animationDuration.value}ms`);
+const movingRight = computed(() => variant.value.endsWith("E"));
+const movingDown = computed(() => variant.value.startsWith("S"));
+
+const startY = computed(() => movingDown.value ? "-40%" : "340%");
+const endX = computed(() => movingRight.value ? "40%" : "-40%");
+const endY = computed(() => movingDown.value ? "170%" : "-20%");
 </script>
 
-<style>
-.travelling-message-leave-active {
-  transition: opacity 0.5s ease;
+<style scoped>
+div { /* carefully not overriding the specificity of the exit animation */
+  animation: travellingMessage forwards v-bind(animationDurationInMs);
 }
 
-.travelling-message-leave-to {
-  opacity: 0;
+@keyframes travellingMessage {
+  0% {
+    transform: translateY(v-bind(startY));
+  }
+
+  50% {
+    transform: translate(0%, v-bind(endY));
+  }
+
+  100% {
+    transform: translate(v-bind(endX), v-bind(endY));
+  }
+}
+
+.travelling-message-leave-active {
+  animation: animatedTravellingMessageExit 0.5s;
+}
+
+@keyframes animatedTravellingMessageExit {
+  0% {
+    transform: translate(v-bind(endX), v-bind(endY));
+  }
+
+  100% {
+    transform: translate(v-bind(endX), v-bind(endY));
+    opacity: 0;
+  }
 }
 </style>
