bfa0733d80a39b153d5714f8d81adf9f37125b95
Joshua Hull <josh.hull.w@gmail.com>
2023-03-27 22:17:00 +1100

    chore: simplify hero-icon-resolver

--- libs/hero-icon-resolver/index.js
+++ libs/hero-icon-resolver/index.js
@@ -3,18 +3,10 @@ const importMap = {
   "IconOutline": "@heroicons/vue/24/outline",
   "SolidIcon": "@heroicons/vue/24/solid",
   "IconSolid": "@heroicons/vue/24/solid",
-  "outline-icon": "@heroicons/vue/24/outline",
-  "icon-outline": "@heroicons/vue/24/outline",
-  "solid-icon": "@heroicons/vue/24/solid",
-  "icon-solid": "@heroicons/vue/24/solid",
   "OutlineMiniIcon": "@heroicons/vue/20/outline",
   "IconMiniOutline": "@heroicons/vue/20/outline",
   "SolidMiniIcon": "@heroicons/vue/20/solid",
   "IconMiniSolid": "@heroicons/vue/20/solid",
-  "outline-mini-icon": "@heroicons/vue/20/outline",
-  "icon-mini-outline": "@heroicons/vue/20/outline",
-  "solid-mini-icon": "@heroicons/vue/20/solid",
-  "icon-mini-solid": "@heroicons/vue/20/solid",
 };
 
 const unkebabify = (str) => str.replace(/-/, "").toUpperCase();
