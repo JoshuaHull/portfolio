00f604ac5870ec899d5e5c98976071825b597297
Joshua Hull <josh.hull.w@gmail.com>
2023-04-02 10:52:53 +1100

    chore(rollup-plugin-html-minify): add tests

---
+++ libs/rollup-plugin-html-minify/rollup-plugin-html-minify.spec.js
@@ -0,0 +1,44 @@
+import { describe, expect, test } from "vitest";
+import {rollupPluginHtmlMinify} from ".";
+
+describe("rollup-plugin-html-minify", () => {
+  test("should resolve null for file not ending in `.html`", () => {
+    // Arrange
+    const resolver = rollupPluginHtmlMinify();
+
+    // Act
+    const result = resolver.transform("<html></html>", "someFile.txt");
+
+    // Assert
+    expect(result).toBeNull();
+  });
+
+  test("should do simple minifications like removing new lines and leading whitespace", () => {
+    // Arrange
+    const resolver = rollupPluginHtmlMinify();
+
+    // Act
+    const result = resolver.transform(
+`<html>
+  <body>
+    <div></div>
+
+    <div></div>
+  </body>
+</html>
+`,
+      "someFile.html"
+    );
+
+    // Assert
+    expect(result.code).toBe(
+`<html>
+<body>
+<div></div>
+<div></div>
+</body>
+</html>
+`
+    );
+  });
+});
