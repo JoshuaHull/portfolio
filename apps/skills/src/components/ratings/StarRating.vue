476f312f4367580e80043c8e347832aefcca7be8
Joshua Hull <josh.hull.w@gmail.com>
2023-04-04 20:51:36 +1000

    feat: add StarRating component

---
+++ apps/skills/src/components/ratings/StarRating.vue
@@ -0,0 +1,63 @@
+<template>
+<div class="star-rating">
+  <slot></slot>
+  <div class="stars">
+    <IconContainer
+      v-for="_ in stars"
+      height="2rem"
+      width="2rem"
+    >
+      <StarSolidIcon />
+    </IconContainer>
+    <IconContainer
+      v-for="_ in empties"
+      height="2rem"
+      width="2rem"
+    >
+      <StarOutlineIcon />
+    </IconContainer>
+  </div>
+</div>
+</template>
+
+<script setup lang="ts">
+interface StarRatingProps {
+  outOf5: number;
+}
+
+const props = defineProps<StarRatingProps>();
+const { outOf5 } = toRefs(props);
+
+const stars = computed(() => {
+  const rtn = [];
+
+  for (let i = 0; i < outOf5.value; i += 1)
+    rtn.push(i);
+
+  return rtn;
+});
+
+const empties = computed(() => {
+  const rtn = [];
+
+  for (let i = 0; i < 5 - outOf5.value; i += 1)
+    rtn.push(i);
+
+  return rtn;
+});
+</script>
+
+<style>
+.star-rating {
+  display: grid;
+  grid-template-rows: auto min-content;
+  row-gap: 0.5rem;
+}
+
+.stars {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  column-gap: 0.5rem;
+  width: min-content;
+}
+</style>
