049e5ae37604b2441ffea4b1bf6a06b680868ad0
Joshua Hull <josh.hull.w@gmail.com>
2023-04-04 21:58:48 +1000

    feat(skills-rest): initial drawer content

--- apps/skills/src/pages/skills-rest-page/SkillsRestDrawerContent.vue
+++ apps/skills/src/pages/skills-rest-page/SkillsRestDrawerContent.vue
@@ -6,6 +6,12 @@
   <StarRating outOf5="5">
     Personal Experience:
   </StarRating>
+  <p>
+    I'm using "REST API" as a catch-all for any API
+    which serves JSON data. I <em>do</em> have
+    professional and personal experience with RESTful APIs,
+    but strictly RESTful APIs are unusual in the real world.
+  </p>
 </div>
 </template>
 
