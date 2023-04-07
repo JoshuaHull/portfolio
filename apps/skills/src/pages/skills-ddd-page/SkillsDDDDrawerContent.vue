63ba8ab7bf8a098c693efa6a23f9650e1ad87ed0
Joshua Hull <josh.hull.w@gmail.com>
2023-04-07 17:14:05 +1000

    feat(skills-ddd): initial drawer content

--- apps/skills/src/pages/skills-ddd-page/SkillsDDDDrawerContent.vue
+++ apps/skills/src/pages/skills-ddd-page/SkillsDDDDrawerContent.vue
@@ -6,6 +6,13 @@
   <StarRating outOf5="4">
     Personal Experience:
   </StarRating>
+  <p>
+    I apply DDD concepts and techniques any time
+    we need to step away from a system's legacy code.
+    DDD is famous for being slow to implement, but
+    long-term it can greatly improve the quality of a
+    feature, even if your starting point is not ideal.
+  </p>
 </div>
 </template>
 
