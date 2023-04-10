2ca70312f92036e95ca133ac965b1bc81b5a6734
Joshua Hull <josh.hull.w@gmail.com>
2023-04-10 15:46:33 +1000

    docs(hero-icon-resolver): add JSDoc comments

--- libs/hero-icon-resolver/hero-icon-resolver.js
+++ libs/hero-icon-resolver/hero-icon-resolver.js
@@ -1,3 +1,7 @@
+/**
+ * @constant
+ * @type {Object.<string, string>}
+ */
 const importMap = {
   "OutlineIcon": "@heroicons/vue/24/outline",
   "IconOutline": "@heroicons/vue/24/outline",
@@ -9,9 +13,32 @@ const importMap = {
   "IconMiniSolid": "@heroicons/vue/20/solid",
 };
 
+/**
+ * @param {string} str
+ * @returns {string}
+ */
 const unkebabify = (str) => str.replace(/-/, "").toUpperCase();
+
+/**
+ * @param {string} str
+ * @returns {string}
+ */
 const toPascal = (str) => str.replace(/(^\w|-\w)/g, unkebabify);
 
+/**
+ * @typedef {Object} ComponentResolveResult
+ * @property {string} name - resolved name of the component
+ * @property {string} from - the path to import the component from
+ */
+
+/**
+ * Tries to resolve a given component name to the correct Hero icon
+ * Returns `undefined` if the component name does not refer to a Hero icon.
+ *
+ * @param {string} componentName - the name of the component as it would appear in the
+ * template of a Vue component
+ * @returns {ComponentResolveResult | undefined}
+ */
 export function heroIconResolver(componentName) {
   const pascalComponentName = toPascal(componentName);
 
