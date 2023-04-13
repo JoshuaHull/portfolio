5233b8a9f8e71c940f74b55cd7ce59c95f05a386
Joshua Hull <josh.hull.w@gmail.com>
2023-04-13 20:52:47 +1000

    docs(use-vanishing-values): add jsdoc comments

--- composables/use-vanishing-values/use-vanishing-values.js
+++ composables/use-vanishing-values/use-vanishing-values.js
@@ -1,5 +1,24 @@
 import { ref } from "vue";
 
+/**
+ * @template [TValue = Object]
+ * @callback PushFn
+ * @param {TValue} nextValue
+ * @returns {void}
+ */
+
+ /**
+  * @template [TValue = Object]
+  * @typedef Ref<TValue>
+  * @property {TValue} value
+  */
+
+/**
+ * @template [TValue = Object]
+ * @param {number} vanishAfterMs - how long each value should last before vanishing
+ * @returns {[Ref<TValue[]>, PushFn]} a Ref of the current values and a function
+ * to push new ephemeral values to the end of the queue
+ */
 export function useVanishingValues(vanishAfterMs) {
   const values = ref([]);
 
