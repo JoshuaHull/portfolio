0215e2039572119b7fa00d6aedf91a3ad4236f9f
JoshuaHull <36806782+JoshuaHull@users.noreply.github.com>
2023-04-16 21:49:04 +1000

    ci(homepage): github action to build and deploy to S3 (#1)

--- apps/homepage/vite.config.js
+++ apps/homepage/vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 import { rollupPluginHtmlMinify } from "rollup-plugin-html-minify";
 
+const warn = console.warn;
+
+console.warn = (message, params) => {
+  if (message.includes(`Generated an empty chunk: "index"`))
+    return;
+
+  warn(message, params);
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
