77aa83b40171b75d42f3d7e69f7dfddb0db923e7
Joshua Hull <josh.hull.w@gmail.com>
2023-09-05 23:41:41 +1000

    fix(web-components): panel didn't work well in NextJs Storybook

--- components/web-components-storybook-nextjs/components/FoldablePanel/FoldablePanel.stories.tsx
+++ components/web-components-storybook-nextjs/components/FoldablePanel/FoldablePanel.stories.tsx
@@ -1,29 +1,37 @@
-import { Meta, StoryObj } from "@storybook/react";
+import { Meta } from "@storybook/react";
 import { FoldablePanelClientOnly } from "../../web-components-wrapper-nextjs/FoldablePanelClientOnly";
+import { FoldablePanelProps } from "@fullstackjosh/web-components/FoldablePanel";
 
 const meta: Meta<typeof FoldablePanelClientOnly> = {
   component: FoldablePanelClientOnly,
 };
 
-export default meta;
-type Story = StoryObj<typeof FoldablePanelClientOnly>;
-
-export const Primary: Story = {
-  render: () => (
-    <FoldablePanelClientOnly
-      defaultChecked={false}
-      panelTitle="Primary Foldable Panel"
-      panelContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
-    />
-  ),
+// dirty hack but something is going on with storybook here.
+// looks like registering components inside the storybook iframe causes a lot of pain.
+// Lit components may have a related issue https://github.com/storybookjs/storybook/issues/12578
+const fixFoldablePanel = (props?: Partial<FoldablePanelProps>) => {
+  setTimeout(() => {
+    const panel: any = document.querySelector("fsj-foldable-panel");
+    panel.panelTitle = props?.panelTitle ?? "Foldable Panel";
+    panel.panelContent = props?.panelContent ?? "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+    panel.defaultChecked = props?.defaultChecked ?? false;
+    panel.checkboxId = props?.checkboxId ?? "1234";
+  });
 };
 
-export const Expanded: Story = {
-  render: () => (
-    <FoldablePanelClientOnly
-      defaultChecked={true}
-      panelTitle="Expanded Foldable Panel"
-      panelContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
-    />
-  ),
+export default meta;
+
+export const Primary = () => {
+  fixFoldablePanel({
+    panelTitle: "Primary Foldable Panel",
+  });
+  return <FoldablePanelClientOnly />;
+};
+
+export const Expanded = () => {
+  fixFoldablePanel({
+    panelTitle: "Expanded Foldable Panel",
+    defaultChecked: true,
+  });
+  return <FoldablePanelClientOnly />;
 };
