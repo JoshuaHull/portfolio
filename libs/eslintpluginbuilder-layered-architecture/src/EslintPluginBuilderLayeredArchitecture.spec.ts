817bb855a49348495d556a4fe7b7cbc3d864c90f
Joshua Hull <josh.hull.w@gmail.com>
2023-09-15 00:02:11 +1000

    feat(epb-layered-architecture): build eslint config from given layers

--- libs/eslintpluginbuilder-layered-architecture/src/EslintPluginBuilderLayeredArchitecture.spec.ts
+++ libs/eslintpluginbuilder-layered-architecture/src/EslintPluginBuilderLayeredArchitecture.spec.ts
@@ -24,4 +24,79 @@ describe("EslintPluginBuilderLayeredArchitecture", () => {
     // Assert
     expect(act).toThrowError(`Layer "third" cannot depend on layer "fourth" as layer "fourth" has not been added to the builder`);
   });
+
+  it("should build a config which prevents importing code into incorrect layers", () => {
+    // Arrange
+    const builder = new EslintPluginBuilderLayeredArchitecture()
+      .withLayer("domain")
+      .withLayer("application", ["domain"])
+      .withLayer("presentation", ["application", "domain"])
+      .withLayer("infrastructure", ["application", "domain"]);
+
+    // Act
+    const result = builder.build("clean-architecture");
+
+    // Assert
+    expect(result).toStrictEqual({
+      plugins: ["clean-architecture"],
+      rules: {
+        "no-restricted-imports": "off",
+      },
+      overrides: [
+        {
+          files: ["**/domain/**"],
+          rules: {
+            "no-restricted-imports": ["error", {
+              patterns: [{
+                group: ["**/application"],
+                message: "Do not import application code into the domain layer",
+              }, {
+                group: ["**/presentation"],
+                message: "Do not import presentation code into the domain layer",
+              }, {
+                group: ["**/infrastructure"],
+                message: "Do not import infrastructure code into the domain layer",
+              }],
+            }],
+          },
+        },
+        {
+          files: ["**/application/**"],
+          rules: {
+            "no-restricted-imports": ["error", {
+              patterns: [{
+                group: ["**/presentation"],
+                message: "Do not import presentation code into the application layer",
+              }, {
+                group: ["**/infrastructure"],
+                message: "Do not import infrastructure code into the application layer",
+              }],
+            }],
+          },
+        },
+        {
+          files: ["**/presentation/**"],
+          rules: {
+            "no-restricted-imports": ["error", {
+              patterns: [{
+                group: ["**/infrastructure"],
+                message: "Do not import infrastructure code into the presentation layer",
+              }],
+            }],
+          },
+        },
+        {
+          files: ["**/infrastructure/**"],
+          rules: {
+            "no-restricted-imports": ["error", {
+              patterns: [{
+                group: ["**/presentation"],
+                message: "Do not import presentation code into the infrastructure layer",
+              }],
+            }],
+          },
+        },
+      ],
+    });
+  });
 });
--- libs/eslintpluginbuilder-layered-architecture/src/EslintPluginBuilderLayeredArchitecture.ts
+++ libs/eslintpluginbuilder-layered-architecture/src/EslintPluginBuilderLayeredArchitecture.ts
@@ -1,28 +1,72 @@
+import { ESLint } from "eslint";
+
 export class EslintPluginBuilderLayeredArchitecture {
-  withLayer<L extends string>(layer: L) {
-    return new LayeredArchitectureBuilder<L, never>(layer, [], new Set<string>([layer]));
+  public withLayer<L extends string>(layer: L) {
+    const layerMap = {} as { [layer in L]: [] };
+    layerMap[layer] = [];
+    return new LayeredArchitectureBuilder<L, never>(layerMap);
   }
 }
 
 class LayeredArchitectureBuilder<T extends string, D> {
   constructor(
-    private layer: T,
-    private dependsOn: D[],
-    private allLayers: Set<string>,
+    private layerMap: { [layer in T]: D[] },
   ) {}
 
-  withLayer<L extends string>(layer: L, dependsOn? : T[]) {
-    if (this.allLayers.has(layer))
+  public withLayer<L extends string>(layer: L, dependsOn? : T[]) {
+    if (this.layerMap.hasOwnProperty(layer))
       throw new Error(`Cannot add layer "${layer}" has it has already been added`);
 
     const deps = dependsOn ?? [];
 
     for (let dep of deps)
-      if (!this.allLayers.has(dep))
+      if (!this.layerMap.hasOwnProperty(dep))
         throw new Error(`Layer "${layer}" cannot depend on layer "${dep}" as layer "${dep}" has not been added to the builder`);
 
-    this.allLayers.add(layer);
+    // type gynmastics :(
+    // TODO: remove typescript
+    const layerMap = this.layerMap as unknown as { [layer in T | L]: T[] };
+    layerMap[layer] = deps;
 
-    return new LayeredArchitectureBuilder<T | L, T>(layer, deps, this.allLayers);
+    return this as unknown as LayeredArchitectureBuilder<T | L, T>;
+  }
+
+  public build(pluginName: string): ESLint.Plugin["configs"] {
+    const overrides: any[] = [];
+
+    for (let layer in this.layerMap) {
+      const deps = this.layerMap[layer];
+      const patterns = [];
+
+      for (let otherLayer in this.layerMap) {
+        if (layer === otherLayer)
+          continue;
+
+        if (deps.includes(otherLayer as any))
+          continue;
+
+        patterns.push({
+          group: [`**/${otherLayer}`],
+          message: `Do not import ${otherLayer} code into the ${layer} layer`,
+        });
+      }
+
+      overrides.push({
+        files: [`**/${layer}/**`],
+        rules: {
+          "no-restricted-imports": ["error", {
+            patterns,
+          }],
+        },
+      });
+    }
+
+    return {
+      plugins: [pluginName],
+      rules: {
+        "no-restricted-imports": "off",
+      },
+      overrides,
+    } as ESLint.Plugin["configs"];
   }
 }
