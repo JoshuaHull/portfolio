215794be901a5a75e284b14cabfb6b9a3c4d107c
Joshua Hull <josh.hull.w@gmail.com>
2023-09-17 20:45:35 +1000

    fix(ep-architected): incorrect plugin name (yes twice)

--- libs/eslint-plugin-architected/src/eslint-plugin-architected.js
+++ libs/eslint-plugin-architected/src/eslint-plugin-architected.js
@@ -7,7 +7,7 @@ const getConfig = () => {
     .withLayer("2-application", ["4-data", "3-domain"])
     .withLayer("1-infrastructure", ["4-data", "3-domain", "2-application"])
     .withLayer("0-presentation", ["4-data", "3-domain", "2-application", "1-infrastructure"])
-    .build("@fulstackjosh/architected");
+    .build("@fullstackjosh/architected");
 };
 
 export default {
