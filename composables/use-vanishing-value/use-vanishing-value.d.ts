6b819d73a88b08f6109607fb8a1b0e45fb2b24bc
Joshua Hull <josh.hull.w@gmail.com>
2023-10-29 15:40:35 +1100

    chore(use-vanishing-value): import TS types into JSDoc comments

--- composables/use-vanishing-value/use-vanishing-value.d.ts
+++ composables/use-vanishing-value/use-vanishing-value.d.ts
@@ -3,4 +3,9 @@ import { Ref } from "vue";
 export type Current<T> = Ref<T | null>;
 export type Push<T> = (value: T) => void;
 
+/**
+ * @param vanishAfterMs how long each value should last before vanishing
+ * @returns a Ref of the current value and a function
+ * to push new ephemeral values to that Ref
+ */
 export function useVanishingValue<T>(vanishAfterMs: number): [Current<T>, Push<T>];
