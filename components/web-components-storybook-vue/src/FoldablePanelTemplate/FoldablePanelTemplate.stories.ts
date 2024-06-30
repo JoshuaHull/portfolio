f32ea92f02eba7ba18b10633984f4ff8fb43bcec
Joshua Hull <josh.hull.w@gmail.com>
2024-07-01 00:42:11 +1000

    feat(web-components): missed dark theme stories for SSR FoldablePanel

--- components/web-components-storybook-vue/src/FoldablePanelTemplate/FoldablePanelTemplate.stories.ts
+++ components/web-components-storybook-vue/src/FoldablePanelTemplate/FoldablePanelTemplate.stories.ts
@@ -18,11 +18,27 @@ export const Primary: Story = {
   },
 };
 
+export const PrimaryDarkTheme: Story = {
+  ...Primary,
+  name: "Primary (Dark Theme)",
+  parameters: {
+    theme: "dark",
+  },
+};
+
 export const Expanded: Story = {
   args: {
     checkboxId: "1234",
     defaultChecked: true,
     panelTitle: "Expanded Foldable Panel",
     panelContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+  },
+};
+
+export const ExpandedDarkTheme: Story = {
+  ...Expanded,
+  name: "Expanded (Dark Theme)",
+  parameters: {
+    theme: "dark",
   },
 };
